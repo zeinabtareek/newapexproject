f1966a68e53cc844d305453e43c1abd98b265599
Heshamazab1999 <60970771+Heshamazab1999@users.noreply.github.com>
2022-02-21 17:28:52 -0800

    Merge remote-tracking branch 'origin/master' into kit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:newapexproject/controller/auth_controller.dart';
 import 'package:newapexproject/routes/app_route.dart';
 import 'package:newapexproject/routes/app_screen.dart';
 import 'package:newapexproject/screens/product_screen/products_screen.dart';
 import 'package:newapexproject/screens/setting_screen/setting_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final controller = Get.put(AuthController());
+
+   MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +39,7 @@ class MyApp extends StatelessWidget {
         builder :()=> GetMaterialApp(
           debugShowCheckedModeBanner: false,
           // home: SettingScreen(),
-          initialRoute: AppRoutes.home,
+          initialRoute: AppRoutes.splashScreen,
           getPages: AppScreens.screens,
         ),
       ),
